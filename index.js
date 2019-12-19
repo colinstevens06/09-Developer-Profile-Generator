cd0384df42eba18eb56f2d82ea7908dabbff74f0
Colin Stevens <colinstevens06@gmail.com>
2019-12-19 18:13:24 -0500

    fixed up html and css templates a bit more

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const sys = require("sys");
-const util = require("util");
+// const sys = require("sys");
+// const util = require("util");
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const PDFDocument = require("pdfkit");
-const download = require("image-downloader");
+const pdf = require("html-pdf");
+const testHTML = fs.readFileSync("./hide/template.html", "utf8");
+const options = { format: "letter", base: "css: ./hide/template.css" };
 
-const pdf = new PDFDocument();
+pdf.create(testHTML, options).toFile("./test.pdf", function(err, res) {
+   if (err) return console.log(err);
+   console.log(res);
+});
 
 let queryURL = "";
 let gitURL = "";
@@ -23,131 +27,58 @@ let gitRepositories, gitFollowers, gitStars, gitUsersFollowing;
 //    // console.log(res.data.login);
 // });
 
-inquirer
-   .prompt([
-      {
-         type: "list",
-         message: "What is your favorite color?",
-         choices: ["Purple", "Blue", "Green", "Yellow", "Orange", "Red"],
-         name: "background"
-      },
-      {
-         type: "input",
-         message: "What is your GitHub username?",
-         name: "username"
-      }
-   ])
-   .then(function(response) {
-      let backgroundColor = response.background;
-      let gitHubUsername = response.username;
-      queryURL = "https://api.github.com/users/" + gitHubUsername;
-      gitURL = "https://www.github.com/" + gitHubUsername;
+// inquirer
+//    .prompt([
+//       {
+//          type: "list",
+//          message: "What is your favorite color?",
+//          choices: ["Purple", "Blue", "Green", "Yellow", "Orange", "Red"],
+//          name: "background"
+//       },
+//       {
+//          type: "input",
+//          message: "What is your GitHub username?",
+//          name: "username"
+//       }
+//    ])
+//    .then(function(response) {
+//       let backgroundColor = response.background;
+//       let gitHubUsername = response.username;
+//       queryURL = "https://api.github.com/users/" + gitHubUsername;
+//       gitURL = "https://www.github.com/" + gitHubUsername;
 
-      switch (backgroundColor) {
-         case "Purple":
-            backgroundHex = "#9933ff";
-            break;
-         case "Blue":
-            backgroundHex = "#0099ff";
-            break;
-         case "Green":
-            backgroundHex = "#00cc66";
-            break;
-         case "Yellow":
-            backgroundHex = "#ffff66";
-            break;
-         case "Orange":
-            backgroundHex = "#ff9933";
-            break;
-         default:
-            backgroundHex = "#ff0066";
-      }
+//       switch (backgroundColor) {
+//          case "Purple":
+//             backgroundHex = "#9933ff";
+//             break;
+//          case "Blue":
+//             backgroundHex = "#0099ff";
+//             break;
+//          case "Green":
+//             backgroundHex = "#00cc66";
+//             break;
+//          case "Yellow":
+//             backgroundHex = "#ffff66";
+//             break;
+//          case "Orange":
+//             backgroundHex = "#ff9933";
+//             break;
+//          default:
+//             backgroundHex = "#ff0066";
+//       }
 
-      axios.get(queryURL).then(function(info) {
-         let gitHub = info.data;
+//       axios.get(queryURL).then(function(info) {
+//          let gitHub = info.data;
 
-         // console.log(gitHub);
+//          // console.log(gitHub);
 
-         avatarURL = gitHub.avatar_url;
-         gitName = gitHub.name;
-         gitLocation = gitHub.location;
-         gitBlog = gitHub.blog;
-         gitBio = gitHub.bio;
-         gitRepositories = gitHub.public_repos;
-         gitFollowers = gitHub.followers;
-         gitUsersFollowing = gitHub.following;
-
-         const options = {
-            url: avatarURL,
-            dest: "./headshots"
-            // filename: `${gitHubUsername}.jpg`
-         };
-
-         console.log(process.argv);
-         download
-            .image(options)
-            .then(({ filename, image }) => {
-               console.log("Saved to", filename);
-            })
-            .catch(err => console.error(err));
-
-         pdf.pipe(fs.createWriteStream("./" + gitHubUsername + ".pdf"));
-         // pdf.image(avatarURL, 5, 5, { width: 200 });
-
-         pdf.rect(0, 10, 650, 100)
-            .lineWidth(3)
-            .fillOpacity(0.2)
-            .fill(backgroundHex);
-
-         pdf.fontSize(25)
-            .fillOpacity(1)
-            .fill("block")
-            .fillColor("black")
-            .text(gitName, 100, 40, { align: "center" });
-
-         pdf.rect(10, 150, 250, 75)
-            .fillOpacity(0.2)
-            .fill(backgroundHex);
-
-         pdf.fontSize(18)
-            .fillOpacity(1)
-            .fillColor("black")
-
-            .fill("block")
-            .text("Public Repositories: " + gitRepositories, 50, 175);
-
-         pdf.rect(10, 250, 250, 75)
-            .fillOpacity(0.2)
-            .fill(backgroundHex);
-
-         pdf.fontSize(18)
-            .fillColor("black")
-
-            .fillOpacity(1)
-            .fill("block")
-            .text("GitHub Stars: " + "?", 50, 250);
-
-         pdf.rect(275, 150, 250, 75)
-            .fillOpacity(0.2)
-            .fill(backgroundHex);
-
-         pdf.fontSize(18)
-            .fillColor("black")
-
-            .fillOpacity(1)
-            .fill("block")
-            .text("Followers: " + gitFollowers, 300, 175);
-
-         pdf.rect(275, 250, 250, 75)
-            .fillOpacity(0.2)
-            .fill(backgroundHex);
-
-         pdf.fontSize(18)
-            .fillColor("black")
-            .fillOpacity(1)
-            .fill("block")
-            .text("Following: " + gitUsersFollowing, 300, 250);
-
-         pdf.end();
-      });
-   });
+//          avatarURL = gitHub.avatar_url;
+//          gitName = gitHub.name;
+//          gitLocation = gitHub.location;
+//          gitBlog = gitHub.blog;
+//          gitBio = gitHub.bio;
+//          gitRepositories = gitHub.public_repos;
+//          gitFollowers = gitHub.followers;
+//          gitUsersFollowing = gitHub.following;
+//       });
+//    });
